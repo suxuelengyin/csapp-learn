f7097a71f5dafbb05bb47af8ddf3433d31f2d2c0
suxue <suxuewb@gmail.com>
2019-04-24 17:46:50 +0800

    侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,28 @@ module.exports = {
     title: "CSAPP 阅读指南",
     head: [
         ['link', { rel: 'icon', href: `/favicon.ico` }],
-    ]
+    ],
+    themeConfig: {
+        editLinks: true,
+        nav: [
+            { text: '首页', link: '/' },
+            { text: 'Blog', link: 'https://www.suxuewb.cn' },
+            { text: 'GitHub', link: 'https://github.com/suxuelengyin/csapp-learn' },
+        ],
+        sidebar: {
+            '/start/': genWhySidebarConfig('为什么')
+        }
+    }
 }
+function genWhySidebarConfig (title) {
+    return [
+      {
+        title,
+        collapsable: false,
+        children: [
+          '',
+          'one',
+        ]
+      }
+    ]
+  }
